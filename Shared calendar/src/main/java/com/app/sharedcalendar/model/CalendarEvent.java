737bdde42efdca1a5a53e3108ef9fc4c1e0daf4f
naseokhoo <nahoo1202@naver.com>
2024-01-29 19:05:08 +0900

    first commit

---
+++ Shared calendar/src/main/java/com/app/sharedcalendar/model/CalendarEvent.java
@@ -0,0 +1,4 @@
+package com.app.sharedcalendar.model;
+
+public class CalendarEvent {
+}
---
+++ Shared calendar/src/main/java/com/app/sharedcalendar/model/User.java
@@ -0,0 +1,35 @@
+package com.app.sharedcalendar.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
+
+
+      private  String email;
+
+      private String name;
+
+      private String password;
+
+
+
+
+}
