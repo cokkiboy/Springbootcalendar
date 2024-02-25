3f6530eab00d2326dd6f90c4fc6e89137b3e578a
naseokhoo <nahoo1202@naver.com>
2024-02-25 09:09:46 +0900

    프론트 엔드 개발진행후 다시백엔드컨트롤러 마무리하기

---
+++ Shared calendar/Shared calendar/src/main/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './Header.css'
+const Header = () => {
+   return(
+
+       <header>
+         <div className="logo">
+            <Link to ="/">
+
+                <img src=""  alt="logo" className='logo'/>
+
+            </Link>
+
+             <div className="util"></div>
+            <ul>
+                <li> </li>
+            </ul>
+         </div>
+
+
+       </header>
+
+   )
+
+
+}
+
+export default Header
