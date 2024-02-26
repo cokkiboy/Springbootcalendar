ac225e8ce38216732eef4ebf765ecf0962ebcb73
naseokhoo <nahoo1202@naver.com>
2024-02-26 20:53:13 +0900

    프론트 엔드 개발 진행중 -home.jsx개발중

--- Shared calendar/Shared calendar/src/main/frontend/src/components/Header/Header.jsx
+++ Shared calendar/Shared calendar/src/main/frontend/src/components/Header/Header.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import './Header.css'
+import {Link} from 'react-router-dom'
 const Header = () => {
    return(
 
        <header>
-         <div className="logo">
-            <Link to ="/">
-
-                <img src=""  alt="logo" className='logo'/>
+        <div  className="logo">
+            <Link to="/">
+              <img  src=""  alt="logo"  className='logo'/>
 
             </Link>
 
-             <div className="util"></div>
-            <ul>
-                <li> </li>
-            </ul>
-         </div>
+        </div>
+         <div  className="util">
+             <ul>
 
+                 <li><Link to="/login" >로그인</Link></li>
+                 <li><Link to="/join" >회원가입</Link></li>
+                 <li><Link to="/about" >소개</Link></li>
+
+             </ul>
+             <ul>
+
+                 <li><Link to="/user" >마이페이지</Link></li>
+                 <li><button className='link'>로그아웃</button></li>
+
+             </ul>
+
+
+         </div>
 
        </header>
 
