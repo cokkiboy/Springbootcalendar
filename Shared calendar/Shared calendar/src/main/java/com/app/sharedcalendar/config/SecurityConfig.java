424aa464fb5eeb24073a77651a6b975a039c8243
naseokhoo <nahoo1202@naver.com>
2024-03-09 23:40:33 +0900

    스프링부트 + 타임리프 공유 캘린더 만들기 인덱스랑 해더 만드는중 컨트롤러와 html연결 하는중

--- Shared calendar/Shared calendar/src/main/java/com/app/sharedcalendar/config/SecurityConfig.java
+++ Shared calendar/Shared calendar/src/main/java/com/app/sharedcalendar/config/SecurityConfig.java
@@ -1,58 +1,50 @@
 package com.app.sharedcalendar.config;
 
 import com.app.sharedcalendar.config.auth.PrincipalDetailService;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean// 빈을 이용하여 직접주입한다
-    public BCryptPasswordEncoder getPasswordEncoder() {
+    private final PrincipalDetailService principalDetailService;
+
+    public SecurityConfig(PrincipalDetailService principalDetailService) {
+        this.principalDetailService = principalDetailService;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(@NotNull AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        return authenticationManagerBuilder.userDetailsService(principalDetailService).passwordEncoder(passwordEncoder()).build();
     }
 
     @Bean
-    SecurityFilterChain filterChain(@NotNull HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin((formLogin) -> formLogin
-                        .loginPage("/")
-                        .defaultSuccessUrl("/index")
-                        .permitAll())
-                .logout((logout) -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/auth/login")
-                        .permitAll())
-                .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/**")))
-        ;
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        HttpSecurity disable = http
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .dispatcherTypeMatchers("/", "/auth/login", "/auth/join").permitAll() // 인증 없이 접근 가능한 URL 설정
+                        .anyRequest().authenticated()) // 나머지 요청은 인증 필요
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/loginForm") // 로그인 페이지 설정
+                        .defaultSuccessUrl("/home") // 로그인 성공 후 이동할 페이지 설정
+                        .permitAll()) // 로그인 페이지는 모든 사용자에게 접근 가능
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // 로그아웃 URL 설정
+                        .logoutSuccessUrl("/auth/loginForm") // 로그아웃 성공 후 이동할 페이지 설정
+                        .permitAll()) // 로그아웃은 모든 사용자에게 접근 가능
+                .csrf().disable();// CSRF 보호 비활성화
+
         return http.build();
     }
-
-
 }
